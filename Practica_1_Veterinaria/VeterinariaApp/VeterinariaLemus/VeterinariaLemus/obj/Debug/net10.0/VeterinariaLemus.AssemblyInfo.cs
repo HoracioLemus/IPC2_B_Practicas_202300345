cdd9a6c5d53c87559611782e97093bfd84b976c2
HoracioLemus <3484300000101@ingenieria.usac.edu.gt>
2026-09-12 20:39:49 -0600

    Configuracion Inicial de la practica y lectura de JSON

--- Practica_1_Veterinaria/VeterinariaApp/VeterinariaLemus/VeterinariaLemus/obj/Debug/net10.0/VeterinariaLemus.AssemblyInfo.cs
+++ Practica_1_Veterinaria/VeterinariaApp/VeterinariaLemus/VeterinariaLemus/obj/Debug/net10.0/VeterinariaLemus.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VeterinariaLemus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09d19853d40e3807080b3a93efd5209992c28692")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7096f1639b8df0779b442bbbd09f3c9710c68d2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("VeterinariaLemus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VeterinariaLemus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ReproductorMusicaECYS/ReproductorMusicaECYS/Cancion.cs
@@ -0,0 +1,17 @@
+namespace ReproductorMusicaECYS;
+using System.Text.Json.Serialization;
+public class Cancion
+{
+    // lectura de Json
+    [JsonPropertyName("titulo")]
+    public string Titulo { get; set; }
+    
+    [JsonPropertyName("artista")]
+    public string Artista { get; set; }
+    
+    [JsonPropertyName("genero")]
+    public string Genero { get; set; }
+    
+    [JsonPropertyName("duracion")]
+    public int Duracion { get; set; }
+}
